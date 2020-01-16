8f8910f0b1e4962514e44b705facd6151c1abb59
andre <andreluizrodper@gmail.com>
2020-01-16 09:57:53 -0300

    Updating Home view
    Adding components, functionalities and css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SectionFilter :selected="selectedSection" />
+    <HighLight
+      :content="item"
+      :index="index"
+      :section="selectedSection"
+      v-for="(item, index) in highlights"
+      :key="index"
+    />
+    <ListItem
+      :content="item"
+      :index="index"
+      :section="selectedSection"
+      v-for="(item, index) in items"
+      :key="index"
+    />
+    <button v-if="hasMore" @click="more()">Load more</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import SectionFilter from '@/components/SectionFilter.vue';
+import HighLight from '@/components/HighLight.vue';
+import ListItem from '@/components/ListItem.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    SectionFilter,
+    HighLight,
+    ListItem,
+  },
+  data() {
+    return {
+      highlights: [],
+      items: [],
+      pagesShown: 10,
+      actualPage: 1,
+      hasMore: false,
+      selectedSection: this.$store.state.section,
+    };
+  },
+  mounted() {
+    this.$store.subscribe((mutation, state) => {
+      if (['toggleList'].indexOf(mutation.type) >= 0) {
+        this.loadList();
+      }
+      if (['toggleSection'].indexOf(mutation.type) >= 0) {
+        this.items = [];
+        this.highlights = [];
+        this.selectedSection = state.section;
+        this.$store.dispatch('getStories');
+        this.actualPage = 1;
+      }
+    });
+    this.loadList();
+  },
+  methods: {
+    more() {
+      const start = this.actualPage * this.pagesShown;
+      const end = start + this.pagesShown;
+      this.actualPage += 1;
+      this.hasMore = this.$store.state.list.length > this.actualPage * this.pagesShown;
+      this.items.push(...this.$store.state.list.slice(start, end));
+    },
+    loadList() {
+      this.highlights = this.$store.state.list.slice(0, 3);
+      this.items = this.$store.state.list.slice(4, 10);
+      this.hasMore = this.$store.state.list.length > this.actualPage * this.pagesShown;
+      this.$store.commit('toggleLoading', false);
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+div {
+  padding-bottom: 20px;
+  button {
+    margin: 0 auto;
+    font-size: 16px;
+    background: #000;
+    color: #fff;
+    border: 1px solid #000;
+    transition: all 0.3s;
+    padding: 15px;
+    width: 200px;
+    display: block;
+    cursor: pointer;
+    &:hover {
+      background: #fff;
+      color: #000;
+    }
+  }
+}
+</style>
